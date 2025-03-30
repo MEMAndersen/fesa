ba02e88a61dd3200f7629d49c22a59a4a3f6231f
MEMAndersen <MEMAndersen@gmail.com>
2025-03-30 21:14:55 +0200

    First commit playing with cvxpy

---
+++ src/examples/rc2.py
@@ -0,0 +1,115 @@
+"""
+Example of Reinforced Concrete material point in 2D with orthogonal reinforcement in two directions (x-y).
+"""
+
+# Import packages.
+import cvxpy as cp
+import numpy as np
+
+
+def main(sigma: list[float], sigma_0: list[float]) -> None:
+    if len(sigma) != 3:
+        raise ValueError(f"Wrong input for sigma, length should be 3, was {len(sigma)}")
+
+    if len(sigma_0) != 3:
+        raise ValueError(
+            f"Wrong input for sigma_0, length should be 3, was {len(sigma_0)}"
+        )
+
+    sigma_tensor = np.array(
+        [
+            [sigma[0], sigma[2]],
+            [sigma[2], sigma[1]],
+        ]
+    )
+
+    sigma_0_tensor = np.array(
+        [
+            [sigma_0[0], sigma_0[2]],
+            [sigma_0[2], sigma_0[1]],
+        ]
+    )
+
+    # Define variables
+    k = 4.0
+    f_t_eff = 0.0  # MPa
+    f_c_eff = 20.0  # MPa
+    phi_x = 5  # MPa
+    phi_y = 5  # MPa
+
+    ## Stresses
+    s_c = cp.Variable((2, 2), symmetric=True, name="S_c")
+    s_s = cp.Variable((2, 2), symmetric=True, name="S_s")
+    s_t = cp.Variable((2, 2), symmetric=True, name="S_t")
+
+    ## Auxiliary
+    s_ii = cp.Variable(1, name="s_ii")
+    s_i = cp.Variable(1, name="s_i")
+    c = cp.Variable(1, name="C")
+    r = cp.Variable(1, name="R")
+    load_factor = cp.Variable(1, name="load_factor")
+    soc_vars = cp.Variable(3)
+
+    # Constraints
+    stress_equality = load_factor * sigma_tensor + sigma_0_tensor == s_t
+    stress_decomposition_constraint = s_t == s_c + s_s
+
+    ## Concrete
+    s_i_eq = s_i == c + r
+    s_ii_eq = s_ii == c - r
+    c_eq = c == 0.5 * (s_c[0, 0] + s_c[1, 1])
+    soc_eqs = [
+        soc_vars[0] == r,
+        soc_vars[1] == 0.5 * (s_c[0, 0] - s_c[1, 1]),
+        soc_vars[2] == s_c[0, 1],
+        cp.SOC(
+            np.array([1, 0, 0]) @ soc_vars,
+            np.array([[0, 0, 0], [0, 1, 0], [0, 0, 1]]) @ soc_vars,
+        ),
+    ]
+
+    mohr_1 = c + r <= f_t_eff
+    mohr_2 = k * (c + r) - (c - r) <= f_c_eff
+    mohr_3 = -(c - r) <= f_c_eff
+
+    ## Steel
+    s_lin_ineq_low = np.zeros((2, 2)) <= s_s
+    s_lin_ineq_high = s_s <= np.diag((phi_x, phi_y))
+
+    # Objective
+    objective = cp.Maximize(load_factor)
+
+    # Create and solve problem
+    prob = cp.Problem(
+        objective=objective,
+        constraints=[
+            stress_equality,
+            stress_decomposition_constraint,
+            s_i_eq,
+            s_ii_eq,
+            c_eq,
+            mohr_1,
+            mohr_2,
+            mohr_3,
+            s_lin_ineq_low,
+            s_lin_ineq_high,
+        ]
+        + soc_eqs,
+    )
+    prob.solve(verbose=True, solver="MOSEK")
+
+    # Print result.
+    print("The optimal value is", prob.value)
+    print(f"S_t = \n{s_t.value}")
+    print(f"S_c = \n{s_c.value}")
+    print(f"S_s = \n{s_s.value}")
+    print(load_factor.value * sigma_tensor + sigma_0_tensor, "\n=\n", s_t.value)
+    print(f"sii = \n{s_ii.value}")
+    print(f"si = \n{s_i.value}")
+
+
+if __name__ == "__main__":
+    sigma = [0, 0, 1]
+    sigma_0 = [0, 0, 0]
+
+    main(sigma, sigma_0)
---
+++ src/examples/rc3.py
@@ -0,0 +1,100 @@
+"""
+Example of Reinforced Concrete material point in 3D with orthorgonal reinforcement in three directions (x-y-z).
+"""
+
+# Import packages.
+import cvxpy as cp
+import numpy as np
+
+
+def main(sigma: list[float], sigma_0: list[float]) -> None:
+    if len(sigma) != 6:
+        raise ValueError(f"Wrong input for sigma, length should be 6, was {len(sigma)}")
+
+    if len(sigma_0) != 6:
+        raise ValueError(
+            f"Wrong input for sigma_0, length should be 6, was {len(sigma_0)}"
+        )
+
+    sigma_tensor = np.array(
+        [
+            [sigma[0], sigma[3], sigma[4]],
+            [sigma[3], sigma[1], sigma[5]],
+            [sigma[4], sigma[5], sigma[2]],
+        ]
+    )
+
+    sigma_0_tensor = np.array(
+        [
+            [sigma_0[0], sigma_0[3], sigma_0[4]],
+            [sigma_0[3], sigma_0[1], sigma_0[5]],
+            [sigma_0[4], sigma_0[5], sigma_0[2]],
+        ]
+    )
+
+    # Define variables
+    k = 4.0
+    f_t_eff = 0.0  # MPa
+    f_c_eff = 20.0  # MPa
+    phi_x = 5  # MPa
+    phi_y = 5  # MPa
+    phi_z = 5  # MPa
+
+    ## Stresses
+    S_c = cp.Variable((3, 3), symmetric=True, name="S_c")
+    S_s = cp.Variable((3, 3), symmetric=True, name="S_s")
+    S_t = cp.Variable((3, 3), symmetric=True, name="S_t")
+
+    ## Auxiliary
+    s3 = cp.Variable(1, name="s3")
+    s1 = cp.Variable(1, name="s1")
+    load_factor = cp.Variable(1, name="load_factor")
+
+    # Constraints
+    stress_equality = load_factor * sigma_tensor + sigma_0_tensor == S_t
+    stress_decomposition_constraint = S_t == S_c + S_s
+
+    ## Concrete
+    sdp_1 = s1 * np.identity(3) - S_c >> 0
+    sdp_2 = s3 * np.identity(3) - S_c << 0
+    lin_ineq_1 = s1 <= f_t_eff
+    lin_ineq_2 = (k * s1 - s3) <= f_c_eff
+
+    ## Steel
+    s_lin_ineq_low = np.zeros((3, 3)) <= S_s
+    s_lin_ineq_high = S_s <= np.diag((phi_x, phi_y, phi_z))
+
+    # Objective
+    objective = cp.Maximize(load_factor)
+
+    # Create and solve problem
+    prob = cp.Problem(
+        objective=objective,
+        constraints=[
+            stress_equality,
+            stress_decomposition_constraint,
+            sdp_1,
+            sdp_2,
+            lin_ineq_1,
+            lin_ineq_2,
+            s_lin_ineq_low,
+            s_lin_ineq_high,
+        ],
+    )
+    prob.solve(verbose=True, solver="MOSEK")
+
+    # Print result.
+    print("The optimal value is", prob.value)
+    print(f"S_t = \n{S_t.value}")
+    print(f"S_c = \n{S_c.value}")
+    print(f"S_s = \n{S_s.value}")
+    print(load_factor.value * sigma_tensor + sigma_0_tensor, "\n=\n", S_t.value)
+    print(f"s3 = \n{s3.value}")
+    print(f"s1 = \n{s1.value}")
+
+
+if __name__ == "__main__":
+    sigma = [-20, -20, -2, 0, 0, 0]
+    sigma_0 = [-10, 0, 0, 0, 0, 0]
+
+    main(sigma, sigma_0)
